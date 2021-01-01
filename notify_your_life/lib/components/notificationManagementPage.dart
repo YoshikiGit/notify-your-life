acf595d3a89b263fbb8a93c9ebc1e9cc1e43b9ab
佐藤利紀 <ysklove.music1stsight@gmail.com>
2021-01-01 14:28:44 +0900

    リストビューの実装

--- notify_your_life/lib/components/notificationManagementPage.dart
+++ notify_your_life/lib/components/notificationManagementPage.dart
@@ -1,27 +1,49 @@
-
-// （ページ1）左ページ
 import 'package:flutter/material.dart';
 
 class NotificationManagementPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // mockデータ
-    const data = [
-      "夕飯いる？", "今日帰宅する？"
-    ];
     return MaterialApp(
       home: Scaffold(
-      body: ListView.builder(
-        itemCount: data.length,
-        itemBuilder: (context, int index) {
-          return Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                data[index],
-              ));
-        },
-      ),
+        body: ListView(
+          children: [
+            menuItem("夕飯いる？", Icon(Icons.notifications_outlined )),
+            menuItem("今日迎え来れる？", Icon(Icons.notifications_outlined )),
+            menuItem("今日帰宅する？", Icon(Icons.notifications_outlined )),
+          ],
+        ),
+        
       ),
     );
   }
+
+  Widget menuItem(String title, Icon icon) {
+    return GestureDetector(
+      child:Container(
+        padding: EdgeInsets.all(8.0),
+        decoration: new BoxDecoration(
+          border: new Border(bottom: BorderSide(width: 1.0, color: Colors.grey))
+        ),
+        child: Row(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child:icon,
+            ),
+            Text(
+              title,
+              style: TextStyle(
+                color:Colors.black,
+                fontSize: 18.0
+              ),
+            ),
+          ],
+        )
+      ),
+      onTap: () {
+        print("onTap called.");
+      },
+    );
+  } 
 }
