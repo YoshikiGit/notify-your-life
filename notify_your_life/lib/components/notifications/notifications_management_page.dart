d9965203118df5275f4dc80588473eba1b1d0606
佐藤利紀 <ysklove.music1stsight@gmail.com>
2021-01-31 21:20:20 +0900

    複数通知のデータ保存を実装

--- notify_your_life/lib/components/notifications/notifications_management_page.dart
+++ notify_your_life/lib/components/notifications/notifications_management_page.dart
@@ -6,32 +6,38 @@ import '../../common/process_common.dart';
 import 'dart:convert';
 
 class NotificationsManagementPage extends StatelessWidget {
-  
+  List<NotificationInfoData> ntfcList = new List<NotificationInfoData>();
+
   @override
   Widget build(BuildContext context) {
-    
-    // mockデータ
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("通知管理"),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.add),
-            padding: const EdgeInsets.all(15.0),
-            onPressed: () => naviRegistModal(context),
-          ),
-        ]
-      ),
-      body: ListView(
-        children: [
-          menuItem("夕飯いる?", context),
-          menuItem("今日迎え来れる？", context),
-          menuItem("今日帰宅する？", context),
-        ],
-      ),  
+    return FutureBuilder(
+      future: executeRead(context),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("通知管理"),
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.add),
+                  padding: const EdgeInsets.all(15.0),
+                  onPressed: () => naviRegistModal(context),
+                ),
+              ]
+            ),
+            body: ListView.builder(
+              itemCount: ntfcList.length,
+              itemBuilder: (context, index) {
+                return menuItem(ntfcList[index].title, context);
+              },
+            )
+          );
+        } else {
+          return Align(child: Text('2秒待機'), alignment: Alignment.topCenter,);
+        }
+      },
     );
   }
-
 
   Widget menuItem(String title, BuildContext context) {
     return GestureDetector(
@@ -43,35 +49,35 @@ class NotificationsManagementPage extends StatelessWidget {
         child:Row(
           children: <Widget>[
             Expanded(
-              flex: 1, child: Container(
-              margin: EdgeInsets.all(10.0),
-              child: Icon(Icons.notifications_outlined),
-            )
+              flex: 1, 
+              child: Container(
+                margin: EdgeInsets.all(10.0),
+                child: Icon(Icons.notifications_outlined),
+              )
             ),
             Expanded(
-              flex: 5, child: Text(
-              title,
-              style: TextStyle(
-                color:Colors.black,
-                fontSize: 18.0
-              ),
-            )
+              flex: 5, 
+              child: Text(
+                title,
+                style: TextStyle(
+                  color:Colors.black,
+                  fontSize: 18.0
+                ),
+              )
             ),
             Expanded(
               flex:1,
               child: RaisedButton(
-              onPressed: () => naviRegistModal(context),
-              padding: const EdgeInsets.all(0.0),
-              child: const Text('編集', style: TextStyle(fontSize: 20)),
+                onPressed: () => naviRegistModal(context),
+                padding: const EdgeInsets.all(0.0),
+                child: const Text('編集', style: TextStyle(fontSize: 20)),
               )
             ),
           ],
         ),
-        
       ),
       onTap: () {
         execute(context);
-
       },
     );
   }
@@ -86,58 +92,25 @@ class NotificationsManagementPage extends StatelessWidget {
     );
   }
 
-  Future<Widget> executeRead (BuildContext context) {
-    return readNotification(context);
+  Future<int> executeRead (BuildContext context) {
+    ntfcList.clear();
+    readNotification(context);
+    return Future.value(1);
   }
 
-  Future<Widget> readNotification (BuildContext context) async {
+  Future readNotification (BuildContext context) async {
+
     final SharedPreferences prefs = await SharedPreferences.getInstance();
-    print(prefs.getString('test'));
-    Map<String, dynamic>  json = jsonDecode(prefs.getString('test'));
-    print(json);
-    var ntcfInfo =  new NotificationInfoData.fromJson(json);
-    print(ntcfInfo.title.toString());
+    print(prefs.getString('ntfcList'));
 
-
-    return GestureDetector(
-      child:Container(
-        padding: EdgeInsets.all(8.0),
-        decoration: new BoxDecoration(
-          border: new Border(bottom: BorderSide(width: 1.0, color: Colors.grey))
-        ),
-        child:Row(
-          children: <Widget>[
-            Expanded(
-              flex: 1, child: Container(
-              margin: EdgeInsets.all(10.0),
-              child: Icon(Icons.notifications_outlined),
-            )
-            ),
-            Expanded(
-              flex: 5, 
-              child: Text(
-              ntcfInfo.title,
-              style: TextStyle(
-                color:Colors.black,
-                fontSize: 18.0
-              ),
-            )
-            ),
-            Expanded(
-              flex:1,
-              child: RaisedButton(
-              onPressed: () => naviRegistModal(context),
-              padding: const EdgeInsets.all(0.0),
-              child: const Text('編集', style: TextStyle(fontSize: 20)),
-              )
-            ),
-          ],
-        ),
-      ),
-      onTap: () {
-        execute(context);
-      },
-    );  
+    if (prefs.getString('ntfcList') != null) {
+      print(jsonDecode(prefs.getString('ntfcList')));
+      // 保存した通知リストの呼び出し
+      print({jsonDecode(prefs.getString('ntfcList')) is List<dynamic>});
+      List<dynamic> json = jsonDecode(prefs.getString('ntfcList'));
+      print(json);
+      ntfcList = json.map((i) => new NotificationInfoData.fromJson(i)).toList();
+    }
   }
 
   void naviRegistModal (BuildContext context) {
@@ -148,6 +121,6 @@ class NotificationsManagementPage extends StatelessWidget {
         builder: (BuildContext context) => RegistNotificationPage(),
         fullscreenDialog: true,
         ),
-      );
+    );
   }  
 }
